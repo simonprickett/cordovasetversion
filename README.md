61c4fdb44c6de7d9d23aaccb795743744c64b2e1
Simon Prickett <simon@crudworks.org>
2015-11-23 16:18:31 -0800

    Updated to make it a bit more flexible and run against a specified config.xml

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ cordovasetversion
 =================
 
 Python Script to set the version number of a Apache Cordova application for iOS and Android
+
+Usage:
+
+```
+setcordovaversion.py 1.9.9 9999 /path/to/config.xml
+```
--- setcordovaversion.py
+++ setcordovaversion.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python 
+
 #####
 #
 # Purpose: Set version and build numbers for Cordova app
@@ -43,53 +45,16 @@ def isValidCordovaBuildString(buildString):
 
 #####
 #
-# Check if the current folder is a Cordova folder
-#
-#####
-def isCordovaProjectFolder():
-	# Check for a set of folders that looks like a Cordova project
-	files = [f for f in os.listdir('.') if os.path.isdir(f)]
-	platformsFound = False
-	pluginsFound = False
-	wwwFound = False
-	hooksFound = False
-
-	for f in files:
-		if (f == 'platforms'):
-			platformsFound = True
-		if (f == 'hooks'):
-			hooksFound = True
-		if (f == 'www'):
-			wwwFound = True
-		if (f == 'plugins'):
-			pluginsFound = True
-
-	# Check for a config.xml too then we can be pretty confident 
-	# is is the right place!
-	configXmlFound = os.path.isfile('config.xml')
-
-	return (configXmlFound and platformsFound and pluginsFound and hooksFound and wwwFound)
-
-#####
-#
-# Check if the project has a specific platform installed
-#
-#####
-def hasPlatform(platformToCheck):
-	return (os.path.isdir('platforms') and os.path.isdir('platforms/' + platformToCheck))
-
-#####
-#
 # Open project's config.xml file and set the version number 
 # and build number according to the parameters.
 #
 #####
-def setVersionAndBuildNumbers(versionNumber, buildNumber):
+def setVersionAndBuildNumbers(versionNumber, buildNumber, configFile):
 	# Determine which platforms this project has, supports 
 	# iOS and Android at the moment
 
 	print 'Opening project\'s config.xml file'
-	xmlTree = minidom.parse('config.xml')
+	xmlTree = minidom.parse(configFile)
 	widgetElem = xmlTree.getElementsByTagName('widget')
 
 	if (len(widgetElem) == 1):
@@ -102,31 +67,27 @@ def setVersionAndBuildNumbers(versionNumber, buildNumber):
 	print 'Setting version number to ' + versionNumber
 	widgetElem.setAttribute('version', versionNumber)
 
-	if (hasPlatform('ios')):
-		print 'Setting iOS build number to ' + buildNumber
-		widgetElem.setAttribute('ios-CFBundleVersion', buildNumber)
+	print 'Setting iOS build number to ' + buildNumber
+	widgetElem.setAttribute('ios-CFBundleVersion', buildNumber)
 	
-	if (hasPlatform('android')):
-		print 'Setting Android build number to ' + buildNumber
-		widgetElem.setAttribute('android-versionCode', buildNumber)
+	print 'Setting Android build number to ' + buildNumber
+	widgetElem.setAttribute('android-versionCode', buildNumber)
 
 	# Persist the XML back to config.xml as utf-8
-	with codecs.open('config.xml', 'wb', 'utf-8') as out:
+	with codecs.open(configFile, 'wb', 'utf-8') as out:
 		xmlTree.writexml(out, encoding='utf-8')
 
 #####
 # Entry point, run the script...
 #####
-if (len(sys.argv) == 3):
-	# Correct number of arguments, check if this is a 
-	# Cordova project directory...
-	if (isCordovaProjectFolder()):
+if (len(sys.argv) == 4):
+	if (os.path.isfile(sys.argv[3])):
 		# Check that first arugment is valid, Cordova needs 0.0.0 type format
 		if (isValidCordovaVersionString(sys.argv[1])):
 			# Check that the second argument is valid, Cordova needs a number
 			if (isValidCordovaBuildString(sys.argv[2])):
-				setVersionAndBuildNumbers(sys.argv[1], sys.argv[2])
-				print 'config.xml updated successfully'
+				setVersionAndBuildNumbers(sys.argv[1], sys.argv[2], sys.argv[3])
+				print sys.argv[3] + ' updated successfully'
 			else:
 				print '*****ERROR: buildNumber must be an integer number'
 				sys.exit(1)
@@ -134,8 +95,7 @@ if (len(sys.argv) == 3):
 			print '*****ERROR: versionNumber must be of the form 0.0.0'
 			sys.exit(1)
 	else:
-		print '*****ERROR: This script must be run inside a Cordova project folder'
-		sys.exit(1)
+		print '*****ERROR: ' + sys.argv[3] + ' file does not exist'
 else:
-	print '*****ERROR: Expecting 2 parameters: versionNumber buildNumber'
+	print '*****ERROR: Expecting 3 parameters: versionNumber buildNumber config.xml'
 	sys.exit(1)
